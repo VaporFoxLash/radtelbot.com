7e7be4cc71f0179e5c3a4bec3477e25b97e0c598
VaporFoxLash <ldonald298@gmail.com>
2020-11-26 08:30:02 +0500

    added a list of commands

--- radtelbot.com/src/main/java/radtelbot.java
+++ radtelbot.com/src/main/java/radtelbot.java
@@ -3,45 +3,69 @@ import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-//import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-//import org.telegram.telegrambots.meta.api.objects.Update;
-//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class radtelbot extends org.telegram.telegrambots.bots.TelegramLongPollingBot{
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+public class radtelbot extends TelegramLongPollingBot{
 
 
     public String getBotUsername() {
-        return "radtelebjdjfot";
+        return "RadteleBot";
     }
 
     public String getBotToken() {
-        return "428789405:AAHeQGtTmq34PYdijLUEb16SVgARgLDXPNA";
+        return "1486539422:AAH2Po2rRY--tVsizLegJXlcO0KJ-2I5s38";
     }
 
 
     public void onUpdateReceived(Update update) {
-        String command=update.getMessage().getText();
 
+        String command=update.getMessage().getText();
 
         SendMessage message = new SendMessage();
 
+        LinkedHashMap<String, String> command_list =
+                new LinkedHashMap<String, String>();
+        command_list.put("Display your username", "/name");
+        command_list.put("Display your fullname", "/fullname");
+        command_list.put("For help", "/help");
 
-        if(command.equals("/myname")){
+
+        System.out.println("Welcome message");
+
+
+        if(command.equals("/start")){
+
+            System.out.println("Display name");
+
+            message.setText("To control the bot use the following commands:");
+//            Iterator iterator = command_list.entrySet().iterator();
+//            while (iterator.hasNext()) {
+//                command_list.ntry me2 = (command_list.Entry) iterator.next();
+//                System.out.println("Key: "+me2.getKey() + " & Value: " + me2.getValue());
+//            }
+
+            String s = command_list.toString();
+            message.setText( s );
+
+        }
+
+        if(command.equals("/name")){
 
             System.out.println(update.getMessage().getFrom().getFirstName());
 
             message.setText(update.getMessage().getFrom().getFirstName());
         }
 
-        if (command.equals("/mylastname")){
-
-            System.out.println(update.getMessage().getFrom().getLastName());
-            message.setText(update.getMessage().getFrom().getLastName());
-        }
-
-        if (command.equals("/myfullname")){
+        if (command.equals("/fullname")){
             System.out.println(update.getMessage().getFrom().getFirstName()+" "+update.getMessage().getFrom().getLastName());
             message.setText(update.getMessage().getFrom().getFirstName()+" "+update.getMessage().getFrom().getLastName());
+        }
+
+        if (command.equals("/help")){
+
+            // To Do
         }
 
         message.setChatId(String.valueOf(update.getMessage().getChatId()));
