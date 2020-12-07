2480a8e5be076104584c3c5092e37a1a40e6f05f
VaporFoxLash <ldonald298@gmail.com>
2020-12-07 18:08:57 +0500

    Make the project compatible with java 11

--- src/main/java/Trivia.java
+++ src/main/java/Trivia.java
@@ -1,27 +1,2 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class Trivia extends Radtelbot {
-
-    private String question;
-    private int score;
-    Map<String, String> questionsMap = new HashMap<>();
-
-    public void setQuestion(String question){
-        this.question = question;
-    }
-
-    public String getQuestion(){
-        return question;
-    }
-
-    public void setScore(int score){
-        this.score = score;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-//    public
+package PACKAGE_NAME;public interface Trivia {
 }
